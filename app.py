668a57ff9ca47080fb9a93571dd9feac083645be
ash-ali <932834897@qq.com>
2021-03-11 20:49:38 +0800

    删除app.py无效代码段

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from importlib import import_module
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
-    from camera import Camera
+    from yolov5_flask import Camera
 from flask_cors import *
 
 app = Flask(__name__)
@@ -21,52 +21,19 @@ DETECTION_FOLDER = r'./static/detections'
 #app.config['DETECTION_FOLDER'] = DETECTION_FOLDER
 
 
-# @app.route("/")
-# def index():
-#     return render_template("index1.html")
-#
-# @app.route("/about")
-# def about():
-#     print("I about")
-#     return render_template("about.html")
-#
-# @app.route("/uploaded",methods = ["GET","POST"])
-# def uploaded():
-#     if request.method == 'POST':
-#         f = request.files['file']
-#         filename = secure_filename(f.filename)
-#         print(filename)
-#         file_path = os.path.join(r"./static/uploads", filename)
-#         print(file_path)
-#         f.save(file_path)
-#         with torch.no_grad():
-#             detect_image(file_path, DETECTION_FOLDER)
-#         return render_template("uploaded.html",display_detection = filename, fname = filename)
 
-# @app.route("/video")
-# def about2():
-#     print("I about")
-#     os.system('python detect.py --source 0')
-#     return render_template("about.html")
 
-@app.route('/woer/detection')
+@app.route('/index')
 def index():
     """Video streaming home page."""
-    return render_template('woer/detection.html')
+    return render_template('index.html')
 
 def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
-        # a = camera.people_appeal()
-        # print('a:{}0'.format(a))
-        # for i in a:
-        #     if i =='people':
-        #         print('是people：{}}')
-        #         people_appeal()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
 @app.route('/video_feed')
 def video_feed():
@@ -77,57 +44,6 @@ def video_feed():
 
 
 
-'''前台页面跳转'''
-@app.route('/woer/index')
-def woer_index():
-    return render_template('woer/index.html')
 
-@app.route('/woer/Batting-01')
-def woer_Batting_01():
-    return render_template('woer/Batting-01.html')
-
-@app.route('/woer/Batting-02')
-def woer_Batting_02():
-    return render_template('woer/Batting-02.html')
-
-@app.route('/woer/Batting-03')
-def woer_Batting_03():
-    return render_template('woer/Batting-03.html')
-
-@app.route('/woer/more-produce')
-def more_produce():
-    return render_template('woer/more-produce.html')
-
-@app.route('/woer/intro-business')
-def intro_business():
-    return render_template('woer/intro-business.html')
-
-@app.route('/woer/Cervical-curve-adjusting-pillow')
-def Cervical_curve_adjusting_pillow():
-    return render_template('woer/Cervical curve adjusting pillow.html')
-
-@app.route('/woer/Luxury-beauty-pillow')
-def Luxury_beauty_pillow():
-    return render_template('woer/Luxury beauty pillow.html')
-
-@app.route('/woer/noon-break-pillow')
-def noon_break_pillow():
-    return render_template('woer/noon-break-pillow.html')
-
-@app.route('/woer/Pillow')
-def Pillow():
-    return render_template('woer/Pillow.html')
-
-@app.route('/woer/pillow-for-winter-and-summer')
-def pillow_for_winter_and_summer():
-    return render_template('woer/pillow for winter and summer.html')
-
-@app.route('/woer/Travel-baby-three-piece-set')
-def Travel_baby_three_piece_set():
-    return render_template('woer/Travel baby three piece set.html')
-
-@app.route('/woer/Washable-pillow')
-def Washable_pillow():
-    return render_template('woer/Washable pillow.html')
 if __name__ == "__main__":
     app.run(debug = True)
